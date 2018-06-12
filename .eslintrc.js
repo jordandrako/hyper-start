600bf67545e3161104b8496abb989b382b3c0c65
Jordan Janzen <jordancjanzen@gmail.com>
2018-06-12 16:49:13 -0700

    Disable eslint for now.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,22 @@
-module.exports = {
-  extends: [
-    'standard',
-    'prettier',
-    'prettier/standard'
-  ],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      { 'singleQuote': true, 'trailingComma': 'es5' }
-    ]
-  },
-  globals: {
-    afterAll: true,
-    afterEach: true,
-    beforeAll: true,
-    beforeEach: true,
-    describe: true,
-    expect: true,
-    fdescribe: true,
-    fit: true,
-    it: true,
-    jest: true,
-    xdescribe: true,
-    xit: true
-  }
-}
+// module.exports = {
+//   extends: ['standard', 'prettier', 'prettier/standard'],
+//   plugins: ['prettier'],
+//   rules: {
+//     'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'es5' }],
+//     'no-console': false,
+//   },
+//   globals: {
+//     afterAll: true,
+//     afterEach: true,
+//     beforeAll: true,
+//     beforeEach: true,
+//     describe: true,
+//     expect: true,
+//     fdescribe: true,
+//     fit: true,
+//     it: true,
+//     jest: true,
+//     xdescribe: true,
+//     xit: true,
+//   },
+// };
