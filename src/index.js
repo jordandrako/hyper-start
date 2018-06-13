988d742fe49669cd1d58d9985c40db02a4b8b9c0
Jordan Janzen <jordancjanzen@gmail.com>
2018-06-12 18:04:36 -0700

    WIP index: decorateConfig, decorateMenu

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import {merge} from 'lodash';
+import dm from './decorateMenu';
+
+const defaultConfig = {
+  hotkeys: {
+    shellPicker: 'ctrl+alt+t'
+  }
+};
+
+let config = defaultConfig;
+let mainShell = {};
+
+export const decorateConfig = mainConfig => {
+  if (mainConfig.shell) {
+    const {shell, shellArgs, env} = mainConfig;
+    mainShell = {
+      default: {
+        name: 'Default Shell',
+        shell,
+        shellArgs,
+        env
+      }
+    };
+  }
+
+  if (mainConfig.hyperStart) {
+    config = merge(mainConfig.hyperStart, JSON.parse(JSON.stringify(defaultConfig)));
+
+    config.shells = mainShell;
+
+    if (mainConfig.hyperStart.shells) {
+      config.shells = merge(mainShell, mainConfig.hyperStart.shells);
+    }
+  }
+  return mainConfig;
+};
+
+export const middleware = () => next => action => {
+  switch (action.type) {
+    case 'CONFIG_LOAD':
+    case 'CONFIG_RELOAD':
+      if (action.config.hyperStart) {
+        config = merge(JSON.parse(JSON.stringify(defaultConfig)), action.config.hyperStart);
+      }
+      break;
+  }
+  return next(action);
+};
+
+export const decorateMenu = menu => dm(open, menu);
